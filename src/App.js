d381344f08befe2a73f0ae762443a07099957baf
rangga <ranggaaldosas@gmail.com>
2023-01-02 22:52:42 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h2 className="text-3xl font-semibold">React + Tailwind Boilerplate</h2>
+    </div>
+  );
+}
+
+export default App;
